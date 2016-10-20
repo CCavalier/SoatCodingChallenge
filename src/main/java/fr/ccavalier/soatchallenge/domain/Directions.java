e6bd3dc4eefbbc29b4419ec4c1b9fea94d70ff0a
Christophe <christof337@hotmail.fr>
2016-10-20 20:38:55 +0200

    Ajout des directions

---
+++ src/main/java/fr/ccavalier/soatchallenge/domain/Directions.java
@@ -0,0 +1,8 @@
+package fr.ccavalier.soatchallenge.domain;
+
+public class Directions {
+	public static int HAUT = 2;
+	public static int BAS = 4;
+	public static int GAUCHE = 1;
+	public static int DROITE = 3;
+}
