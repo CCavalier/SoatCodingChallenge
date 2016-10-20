1a7e0c5bdb1f04a5a52b4ba94c8991b7d25a6920
Christophe <christof337@hotmail.fr>
2016-10-20 21:28:11 +0200

    Commit drone

--- src/main/java/fr/ccavalier/soatchallenge/domain/Drone.java
+++ src/main/java/fr/ccavalier/soatchallenge/domain/Drone.java
@@ -2,6 +2,8 @@ package fr.ccavalier.soatchallenge.domain;
 
 import java.util.List;
 
+import fr.ccavalier.soatchallenge.service.util.Utils;
+
 public class Drone {
 	
 	/**
@@ -73,12 +75,58 @@ public class Drone {
 		this.nbDeplacementsRestants = nbDeplacementsRestants;
 	}
 
-	public void move(Coordonnee position2) {
+	public void move(Coordonnee positionCible) {
 		// penser à décrémenter le fucking nb
+		List<Integer> deplacements = Utils.getChemin(this.getPosition(), positionCible);
+		int distance = deplacements.size();
 		
+		if(distance>this.getNbDeplacementsRestants() || distance>this.getNbToursRestants()) {
+			System.err.println("c'est la merde on peut pas aller là mec!"); 
+		}else {
+			// le déplacement est un succès
+			this.setPosition(positionCible);
+			this.listeDeplacements.addAll(deplacements);
+			this.setNbColisRestants(this.getNbColisRestants()-1);
+			this.setNbDeplacementsRestants(this.getNbDeplacementsRestants()-distance);
+		}
 	}
 	
 	public int getNbToursRestants() {
 		return Map.map.getNbTours() - this.listeDeplacements.size();
+	}
+
+	public void drop(Colis colisLePlusProche) {
+		if(!colisLePlusProche.getPosition().equals(this.getPosition())) {
+			System.err.println("putain on peut pas drop un colis si on est pas dessus!"); 
+		}else if(colisLePlusProche.isEstLivre()){
+			System.err.println("merde il a déjà été livré...");
+		}else {
+			// le drop est un succès
+			colisLePlusProche.setEstLivre(true);
+			this.setNbColisRestants(this.getNbColisRestants()-1);
+		}
+	}
+	
+	/**
+	 * comble les déplacements avec des 0
+	 */
+	public void formaterPourEcrire() {
+		for(int i=0;i<this.getNbDeplacementsRestants();i++) {
+			this.listeDeplacements.add(Directions.STANBY);
+		}
+	}
+
+	/**
+	 * @return the listeDeplacements
+	 */
+	public List<Integer> getListeDeplacements() {
+		return listeDeplacements;
+	}
+
+	/**
+	 * @param listeDeplacements the listeDeplacements to set
+	 */
+	public void setListeDeplacements(List<Integer> listeDeplacements) {
+		this.listeDeplacements = listeDeplacements;
 	}
 }
